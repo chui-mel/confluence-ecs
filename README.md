ea575b75e7c41a3b4d4f0c909304e19bf702d6a2
Chui Melbourne <chui.melbourne@gmail.com>
2020-09-07 21:25:36 +1000

    added aws cli command to get public ip information

--- README.md
+++ README.md
@@ -15,3 +15,30 @@ Refer to [AWS CDK Toolkit (cdk command)](https://docs.aws.amazon.com/cdk/latest/
 1. install nodejs (better to use the latest stable one)
 2. install AWS CDK `npm install -g aws-cdk`
 3. configure your aws token locally by using aws cli
+
+## deployment process:
+1. `mvn package`
+2. `cdk synth`
+3. `cdk deploy'
+
+## How to get public IP for ECS task
+- install jq 
+- run the following command to get public ip list with vpcId and eniId
+  if you have only one task running, then that public ip is you need.
+  ```
+   aws ec2 describe-network-interfaces | jq '.NetworkInterfaces | .[] | (.NetworkInterfaceId, .VpcId, .Association.PublicIp)'
+  ```
+- If you want to get more information about the ECS tasks, run the following command
+    ```
+    aws ecs list-clusters | jq '.clusterArns' | grep ConfluenceEcsStack
+  
+    aws ecs list-tasks --cluster ConfluenceEcsStack-ConfluenceClusterDE24D643-N2QyEvSMDuwJ | jq '.taskArns'
+    
+    aws ecs describe-tasks --cluster ConfluenceEcsStack-ConfluenceClusterDE24D643-N2QyEvSMDuwJ --tasks 78582e64-17ec-4a14-a471-0f07d9955d27 | jq '.tasks | .[0] | .attachments| .[0] | .details'
+    ```
+  
+Note: 'ConfluenceEcsStack-ConfluenceClusterDE24D643-N2QyEvSMDuwJ' and '78582e64-17ec-4a14-a471-0f07d9955d27' are examples for the command returns.
+
+## Watch out when deleting cloudformation stack
+- when you finish deleting cloudformation stack (whatever way), remember to check cloudwatch log group, 
+because the log group retains longer than your expectation. They might be there and fail your next CDK deployment.
--- src/main/java/com/myorg/ConfluenceEcsStack.java
+++ src/main/java/com/myorg/ConfluenceEcsStack.java
@@ -17,7 +17,7 @@ public class ConfluenceEcsStack extends Stack {
     public ConfluenceEcsStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        Vpc vpc = Vpc.Builder.create(this, "MyVpc")
+        Vpc vpc = Vpc.Builder.create(this, "ConfluenceVpc")
             .maxAzs(2)  // Default is all AZs in region
             .natGateways(0)
             .subnetConfiguration(List.of(
@@ -33,22 +33,56 @@ public class ConfluenceEcsStack extends Stack {
         securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8090));
         securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(8091));
 
-        Cluster cluster = Cluster.Builder.create(this, "MyCluster")
+        Cluster cluster = Cluster.Builder.create(this, "ConfluenceCluster")
             .vpc(vpc)
             .build();
 
+//        setupService("ConfluenceServer772", "confluence-772-test", "atlassian/confluence-server:7.7.2",
+//            cluster, List.of(securityGroup));
+//        setupService("ConfluenceServer773", "confluence-773-test", "atlassian/confluence-server:7.7.3",
+//            cluster, List.of(securityGroup));
+//
+        stopTaskInService("ConfluenceServer772", "confluence-772-test", "atlassian/confluence-server:7.7.2",
+            cluster, List.of(securityGroup));
+        stopTaskInService("ConfluenceServer773", "confluence-773-test", "atlassian/confluence-server:7.7.3",
+            cluster, List.of(securityGroup));
+    }
+
+    private FargateService setupService(String serviceId, String taskId, String imageName,
+                                        Cluster cluster, List<ISecurityGroup> sgs) {
+        return FargateService.Builder.create(this, serviceId)
+            .cluster(cluster)
+            .securityGroups(sgs)
+            .taskDefinition(defineTask(taskId, imageName))
+            .desiredCount(1)
+            .assignPublicIp(true)
+            .build();
+    }
+
+    private FargateService stopTaskInService(String serviceId, String taskId, String imageName,
+                                        Cluster cluster, List<ISecurityGroup> sgs) {
+        return FargateService.Builder.create(this, serviceId)
+            .cluster(cluster)
+            .securityGroups(sgs)
+            .taskDefinition(defineTask(taskId, imageName))
+            .desiredCount(0)
+            .assignPublicIp(true)
+            .build();
+    }
+
+    private FargateTaskDefinition defineTask(String taskId, String imageName) {
         FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder
-            .create(this, "confluence-test")
+            .create(this, "confluence-test" + taskId)
             .memoryLimitMiB(4096)
             .cpu(2048)
             .build();
 
-        ContainerDefinition containerDefinition = taskDefinition.addContainer("confluence-container",
+        ContainerDefinition containerDefinition = taskDefinition.addContainer("confluence-" + taskId ,
             ContainerDefinitionOptions.builder()
-                .image(ContainerImage.fromRegistry("atlassian/confluence-server"))
+                .image(ContainerImage.fromRegistry(imageName))
                 .logging(LogDriver.awsLogs(AwsLogDriverProps.builder()
-                    .streamPrefix("confluence-cluster")
-                    .logGroup(LogGroup.Builder.create(this, "confluence-test-log").logGroupName("ecs-confluence").build())
+                    .streamPrefix("confluence-" + taskId)
+                    .logGroup(LogGroup.Builder.create(this, "confluence-log-" + taskId).logGroupName("ecs-confluence" + taskId).build())
                     .build()))
                 .build());
 
@@ -57,13 +91,6 @@ public class ConfluenceEcsStack extends Stack {
             PortMapping.builder().containerPort(8091).hostPort(8091).build()
         );
 
-        FargateService.Builder.create(this, "ConfluenceTestService")
-            .cluster(cluster)
-            .securityGroups(List.of(securityGroup))
-            .taskDefinition(taskDefinition)
-            .desiredCount(1)
-            .assignPublicIp(true)
-            .build();
-
+        return taskDefinition;
     }
 }
